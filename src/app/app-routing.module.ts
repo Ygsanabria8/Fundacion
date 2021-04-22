ff72544953a4a1e8cace503a57f4ef15af484abe
Yesid <yesid@doublevpartners.com-at-074803668141>
2021-04-21 22:06:35 -0500

    seccion de servicios, problema en el carrusel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,9 @@ const routes: Routes = [
           path:'home', loadChildren:()=> import('./home/home.module').then(m => m.HomeModule)
         },
         {
+          path:'services', loadChildren:()=> import('./services/services.module').then((m)=> m.ServicesModule)
+        },
+        {
           path:'about',loadChildren:()=> import('./about/about.module').then(m => m.AboutModule)
         },
       ]
